aa0a779f4991b051dc38f8318647ca5442034c3d
Simon Kostenko <semen.kostenko@cantire.com>
2024-01-16 10:21:40 +0200

    Initial Commit

---
+++ Survey/SurveyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SurveyApp.swift
+//  Survey
+//
+//  Created by Simon Kostenko on 16.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct SurveyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
